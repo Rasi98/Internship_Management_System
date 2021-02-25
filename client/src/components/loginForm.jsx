0f3a9f0f0d9e0e58b87619ff4ca4c5a230e75c0b
lakshan <49824628+Rasi98@users.noreply.github.com>
2021-02-25 21:13:21 +0530

    Login component

---
+++ client/src/components/loginForm.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Loginform extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+
+         }
+    }
+    render() { 
+        return ( 
+            <form>
+                <h1>Hello world!</h1>
+            </form>
+         );
+    }
+}
+ 
+export default Loginform;
---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Loginform from './components/loginForm';
+
+ReactDOM.render(<Loginform/>,document.getElementById('root'));
