48857eeb9dcba2037a0b00e953dcf86eab63ef80
lakshan <49824628+Rasi98@users.noreply.github.com>
2021-02-26 17:33:52 +0530

    Created Login form
    Created structure of the login form using bootstrap

--- client/src/components/loginForm.jsx
+++ client/src/components/loginForm.jsx
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class Loginform extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-
+            username:'',
+            password:''
          }
     }
+
+    handleUsernameChange=(event)=>{
+        this.setState({username:event.target.value});
+    }
+    handlePasswordChange=(event)=>{
+        this.setState({password:event.target.value})
+    }
+    handleSubmit=(event)=>{
+        alert('Login successfull!');
+        event.preventDefault();
+    }
+    
+
     render() { 
         return ( 
-            <form>
-                <h1>Hello world!</h1>
-            </form>
+            <React.Fragment>
+                <h3>
+                    LOGIN
+                <small className="text-muted"> | Internship portal</small>
+                </h3>
+                <form>
+                    <div className='form-group'>        
+                        <label htmlFor="exampleInputEmail1">Username</label>
+                        <input type="text" className="form-control" placeholder="Enter username" value={this,this.state.username} onChange={this.handleUsernameChange}></input>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}></input>
+                    </div>
+                    <button type="submit" className="btn btn-primary" onClick={this.handleSubmit}>Submit</button>
+                </form>
+            </React.Fragment>
          );
     }
 }
